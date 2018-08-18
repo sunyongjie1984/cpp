9a5560453796bd77d90b6599e1a7942a6441b75b
yongjies <yongjies@cadence.com>
2018-08-18 11:44:11 +0800

    modify

---
+++ FunctionPointer/FunctionPointer/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  FunctionPointer
+//
+//  Created by Jack SUN on 16/5/7.
+//  Copyright © 2016年 Jack SUN. All rights reserved.
+//
+
+#include <iostream>
+typedef int (*ONLOADFUN)(int i, int j);
+int (*add4)(int i, int j);
+int add3(int a, int b)
+{
+    return a + b + 3;
+}
+
+int main()
+{
+    ONLOADFUN add4;
+    add4 = add3;
+    std::cout << add4(4, 5) << std::endl;
+    
+    ONLOADFUN add5;
+    add5 = ONLOADFUN(add3);
+    std::cout << add5(4, 5) << std::endl;
+    
+    ONLOADFUN add6;
+    add6 = (ONLOADFUN)add3;
+    std::cout << add6(4, 5) << std::endl;
+    return 0;
+}
